dd9165c96715e9a2d07ba63f39805467be0acc57
nazarsolokha <nazar.solokha@gmail.com>
2017-10-10 19:08:52 +0300

    файл на языке с++

---
+++ main(1).c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+ 
+int main(){
+	for (int i = 0; i < 1; i++){
+		for(int j = 0; j<1; j++){
+			printf("*");
+		}
+		
+	}
+	printf("\n");
+	for (int i = 0; i < 1; i++){
+		for(int j = 0; j<2; j++){
+			printf("*");
+		}
+	printf("\n");
+	for (int i = 0; i < 1; i++){
+		for(int j = 0; j<3; j++){
+			printf("*");
+		}
+		
+	}
+	printf("\n");
+	for (int i = 0; i < 1; i++){
+		for(int j = 0; j<4; j++){
+			printf("*");
+		}
+		
+	}
+	printf("\n");
+	for (int i = 0; i < 1; i++){
+		for(int j = 0; j<5; j++){
+			printf("*");
+		}
+		
+	}
+	printf("\n");
+	for (int i = 0; i < 1; i++){
+		for(int j = 0; j<6; j++){
+			printf("*");
+		}
+	printf("\n");
+	printf("\n");
+	for (int i = 0; i < 1; i++){
+		for(int j = 0; j<6; j++){
+			printf("*");
+		}
+	}
+	}
+	printf("\n");
+	for (int i = 0; i < 1; i++){
+		for(int j = 0; j<5; j++){
+			printf("*");
+		}
+		
+	}
+	printf("\n");
+	for (int i = 0; i < 1; i++){
+		for(int j = 0; j<4; j++){
+			printf("*");
+		}
+		
+	}
+	printf("\n");
+	for (int i = 0; i < 1; i++){
+		for(int j = 0; j<3; j++){
+			printf("*");
+		}
+		
+	}
+	printf("\n");
+	for (int i = 0; i < 1; i++){
+		for(int j = 0; j<2; j++){
+			printf("*");
+		}
+		
+	}
+	printf("\n");
+	for (int i = 0; i < 1; i++){
+		for(int j = 0; j<1; j++){
+			printf("*");
+		}
+		
+	}
+	printf("\n");
+	printf("     ");
+	for (int i = 0; i < 1; i++){
+		for(int j = 0; j<1; j++){
+			printf("*");
+		}
+		
+	}
+	printf("\n");
+	printf("    ");
+	for (int i = 0; i < 1; i++){
+		for(int j = 0; j<3; j++){
+			printf("*");
+		}
+		
+	}
+	printf("\n");
+	printf("   ");
+	for (int i = 0; i < 1; i++){
+		for(int j = 0; j<5; j++){
+			printf("*");
+		}
+		
+	}
+	printf("\n");
+	printf("  ");
+	for (int i = 0; i < 1; i++){
+		for(int j = 0; j<7; j++){
+			printf("*");
+		}
+		
+	}
+	printf("\n");
+	printf(" ");
+	for (int i = 0; i < 1; i++){
+		for(int j = 0; j<9; j++){
+			printf("*");
+		}
+		
+	}
+	printf("\n");
+	
+	for (int i = 0; i < 1; i++){
+		for(int j = 0; j<11; j++){
+			printf("*");
+		}
+		
+	}
+	printf("\n");
+	printf("     ");
+	for (int i = 0; i < 1; i++){
+		for(int j = 0; j<1; j++){
+			printf("*");
+		}
+		
+	}
+	}
+	return 0;
+}
